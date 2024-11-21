a57dc4f3cf77f510b8b9f744c8b054889d6836b3
zzynep <ozeyneprar@gmail.com>
2024-11-21 01:59:35 +0000

    add bouncing box

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,11 +38,11 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform(100, 700, 200, 20);
-    createPlatform(200, 600, 200, 20);
-    createPlatform(300, 500, 200, 20);
-    createPlatform(400, 400, 200, 20);
-    createPlatform(500, 300, 200, 20);
+    createPlatform(300, 700, 1100, 10);
+    createPlatform(400, 600, 1000, 10);
+    createPlatform(500, 500, 1100, 10);
+    createPlatform(600, 400, 1000, 10);
+    createPlatform(700, 300, 1100, 10);
     
     // TODO 2
     // Create collectables
@@ -62,7 +62,7 @@ $(function () {
     // example usage: createCannon(side, position, delay, width, height)
     createCannon("left", 400, 1600);
     createCannon("left", 250, 1000);
-    createCannon("left", 50, 150);
+    createCannon("left", 50, 1500);
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
